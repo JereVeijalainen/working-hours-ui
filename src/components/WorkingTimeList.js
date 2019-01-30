6834c7b8d7d931f78fb7b205e23c7607f18d4070
JereVeijalainen <jereveijalainen82@gmail.com>
2019-01-30 14:52:45 +0200

    Refactor filter dropdown component
    Make FilterDropdown as class component and put toggle in that component's own state.

--- src/components/WorkingTimeList.js
+++ src/components/WorkingTimeList.js
@@ -2,50 +2,58 @@ import React, { Component } from 'react';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import WorkingTimeRow from './WorkingTimeRow';
 
-const FilterDropdown = ({title, selectedItem, itemList, isOpen, toggle, filterFunction}) => {
+/* TODO:
+	- Rivin poisto listalta
+	- Filttereiden toimiminen yhtäaikasesti
+	- Onko kaikki state-tasot järkevästi?
+*/
 
-	return (
-		<ButtonDropdown isOpen={isOpen} toggle={toggle} className="filter-dropdown">
-			<DropdownToggle caret>
-				{selectedItem ? selectedItem : title}
-			</DropdownToggle>
-			<DropdownMenu>
-			<DropdownItem key={0} onClick={() => filterFunction('All')}>All</DropdownItem>
-			{itemList.map((item, i) =>
-				<DropdownItem key={i + 1} onClick={() => filterFunction(item)}>{item}</DropdownItem>
-			)}
-			</DropdownMenu>
-		</ButtonDropdown>
-	);
+class FilterDropdown extends Component {
+
+	constructor(props) {
+    super(props);
+
+    this.toggle= this.toggle.bind(this);		
+    this.state = {
+			dropdownOpen: false,
+    };
+	}
+	
+	toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+	}
+
+	render () {
+		const {title, selectedItem, itemList, filterFunction} = this.props;
+
+		return (
+			<ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} className="filter-dropdown">
+				<DropdownToggle caret>
+					{selectedItem ? selectedItem : title}
+				</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem key={0} onClick={() => filterFunction('All')}>All</DropdownItem>
+				{itemList.map((item, i) =>
+					<DropdownItem key={i + 1} onClick={() => filterFunction(item)}>{item}</DropdownItem>
+				)}
+				</DropdownMenu>
+			</ButtonDropdown>
+		);
+	}
 }
 
 class WorkingTimeList extends Component {
 	constructor(props) {
-    super(props);
-
-    this.toggleWorker = this.toggleWorker.bind(this);
-		this.toggleProject = this.toggleProject.bind(this);
+		super(props);
 		
     this.state = {
 			filteredWorkingTimes: this.props.workingTimes,
-			workerDropdownOpen: false,
-			projectDropdownOpen: false,
 			selectedWorker: '',			
 			selectedProject: ''
     };
   }
-
-  toggleWorker() {
-    this.setState({
-      workerDropdownOpen: !this.state.workerDropdownOpen
-    });
-	}
-
-  toggleProject() {
-    this.setState({
-      projectDropdownOpen: !this.state.projectDropdownOpen
-    });
-	}
 
 	filterByWorker = worker => {
 		const filteredRows = worker && worker !== "All"  ? this.props.workingTimes.filter(time => time.worker === worker) : this.props.workingTimes;
@@ -83,15 +91,11 @@ class WorkingTimeList extends Component {
 					<FilterDropdown title="Worker"
 													selectedItem={this.state.selectedWorker}
 													itemList={workersInWorkingTimes}
-													isOpen={this.state.workerDropdownOpen}
-													toggle={this.toggleWorker}
 													filterFunction={this.filterByWorker} />
 
 					<FilterDropdown title="Project"
 													selectedItem={this.state.selectedProject}
 													itemList={projectsInWorkingTimes}
-													isOpen={this.state.projectDropdownOpen}
-													toggle={this.toggleProject}
 													filterFunction={this.filterByProject} />
 				</section>
 				<section>
