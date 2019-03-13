4f80a486154d256f9b5c884e5151bbe7463d4d54
JereVeijalainen <jereveijalainen82@gmail.com>
2019-03-13 12:26:59 +0200

    Fix texts and indentations of summary page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 const Summary = ({total=0}) => (
-		<div>
-			<div>
-				TEST
-			</div>
-			<div>
-				<div>
-					<span>{total} hours</span>
-				</div>
-			</div>
-		</div>
+  <div>
+    <div>
+      <h4>TEST</h4>
+      <p>This page is under construction</p>
+    </div>
+    <div>
+      <div>
+        <span>{total} hours</span>
+      </div>
+    </div>
+  </div>
 )
 
 export default Summary
