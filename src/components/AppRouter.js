e11de18016b9434b869d8536cb7a86985625812d
JereVeijalainen <jereveijalainen82@gmail.com>
2019-03-25 20:30:49 +0200

    Fix indentations

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from './App';
 
 const AppRouter = () => {
-	return (
-		<Router>
-			<div>
-				<nav className="menu">
-					<Link to="/">Home</Link>
-					<Link to="/add">Add working time</Link>
-					<Link to="/list">Working time list</Link>
-					<Link to="/summary">Summary</Link>
-				</nav>
-				<Route path="/" component={App} />
-			</div>
-		</Router>
-		);
-	}
+  return (
+    <Router>
+      <div>
+        <nav className="menu">
+          <Link to="/">Home</Link>
+          <Link to="/add">Add working time</Link>
+          <Link to="/list">Working time list</Link>
+          <Link to="/summary">Summary</Link>
+        </nav>
+        <Route path="/" component={App} />
+      </div>
+    </Router>
+  );
+}
 
 export default AppRouter
