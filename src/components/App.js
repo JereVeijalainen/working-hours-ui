e1249be5ce7d5aaa289980d070cda04486f25c3f
Jere Veijalainen <jere.veijalainen@unitedbankers.fi>
2025-02-05 12:22:11 +0200

    Move test data to its own file

--- src/components/App.js
+++ src/components/App.js
@@ -5,47 +5,7 @@ import AddWorkingTimeForm from './AddWorkingTimeForm';
 import Summary from './Summary';
 import Home from './Home';
 import { remove } from '../utils/array';
-
-const workingTimes = [
-  {
-    project: 'Rio Working Hours',
-    worker: 'Jere Veijalainen',
-    date: '2019-01-01',
-    hours: 7.5,
-    description: 'Planning and stuff...'
-  },
-  {
-    project: 'Rio Working Hours',
-    worker: 'Mikko Mallikas',
-    date: '2019-01-02',
-    hours: 8,
-    description: 'Designing the UI'
-  },
-  {
-    project: 'Test Project',
-    worker: 'Börje Börgelsson',
-    date: '2019-01-24',
-    hours: 6,
-    description: 'Coding UI'
-  }
-];
-
-const projects = [
-  'Rio Working Hours',
-  'Test Project',
-  'Project X'
-];
-
-const workers = [
-  { firstName: 'Jere', lastName: 'Veijalainen' },
-  { firstName: 'Mikko', lastName: 'Mallikas' },
-  { firstName: 'Maija', lastName: 'Meikäläinen' },
-  { firstName: 'Börje', lastName: 'Börgelsson' }
-];
-
-/* TODO:
-  - Testidatat omaan tiedostoonsa
-*/
+import { workingTimes, projects, workers } from '../data/testData';
 
 class App extends Component {
 
---
+++ src/data/testData.js
@@ -0,0 +1,36 @@
+export const workingTimes = [
+  {
+    project: 'Rio Working Hours',
+    worker: 'Jere Veijalainen',
+    date: '2019-01-01',
+    hours: 7.5,
+    description: 'Planning and stuff...'
+  },
+  {
+    project: 'Rio Working Hours',
+    worker: 'Mikko Mallikas',
+    date: '2019-01-02',
+    hours: 8,
+    description: 'Designing the UI'
+  },
+  {
+    project: 'Test Project',
+    worker: 'Börje Börgelsson',
+    date: '2019-01-24',
+    hours: 6,
+    description: 'Coding UI'
+  }
+];
+
+export const projects = [
+  'Rio Working Hours',
+  'Test Project',
+  'Project X'
+];
+
+export const workers = [
+  { firstName: 'Jere', lastName: 'Veijalainen' },
+  { firstName: 'Mikko', lastName: 'Mallikas' },
+  { firstName: 'Maija', lastName: 'Meikäläinen' },
+  { firstName: 'Börje', lastName: 'Börgelsson' }
+];
